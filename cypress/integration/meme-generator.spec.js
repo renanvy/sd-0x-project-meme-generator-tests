6d21ce3cc1636cd919b40c8b7edc1b3b0255c948
Antonio Britto <antonioabritto@gmail.com>
2020-05-21 00:31:57 -0300

    update specs

--- cypress/integration/meme-generator.spec.js
+++ cypress/integration/meme-generator.spec.js
@@ -27,7 +27,7 @@ const isOverEachother = (backgroundElement, forefrontElement) => {
   );
 };
 
-describe('Meme generator', function() {
+describe('Meme generator', function () {
   const checkFullOverlappage = (backgroundSelector, forefrontSelector) => {
     cy.document().then(doc => {
       const backgroundElement = {
@@ -56,7 +56,7 @@ describe('Meme generator', function() {
     });
   };
 
-  const typeTextAndCheckItsPosition = () => {
+  const typeTextAndCheckItsPosition = (container, child) => {
     // It inserts text in the input and page when typed
     cy.get('#text-input')
       .type('My awesome meme')
@@ -65,7 +65,7 @@ describe('Meme generator', function() {
     cy.contains(/^My awesome meme$/).should('be.visible');
 
     // Has one element inside another
-    checkFullOverlappage('#meme-image-container', '#meme-text');
+    checkFullOverlappage(container, child);
   };
 
   const memeUpload = () => {
@@ -79,23 +79,23 @@ describe('Meme generator', function() {
     });
   };
 
-  it("O site deve possuir uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida. A caixa de texto deve, necessariamente, ter um id denominado 'text-input'. O texto de quem usa deve ser inserido sobre a imagem escolhida. Se não houver imagem inserida, ele deve ser inserido sobre a área onde a imagem aparece na posição correta.", function() {
+  it("O site deve possuir uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida.", function () {
     cy.viewport(1366, 768);
     cy.visit('/');
 
-    typeTextAndCheckItsPosition();
+    typeTextAndCheckItsPosition('#meme-image-container', '#meme-text');
   });
 
-  it('O site deve permitir que quem usa faça upload de uma imagem de seu computador. A imagem escolhida deve ocupar o espaço delimitado pela moldura, assim como seu texto.', function() {
+  it("O site deve permitir que quem usa faça upload de uma imagem de seu computador.", function () {
     cy.viewport(1366, 768);
     cy.visit('/');
 
     memeUpload();
     cy.get('#meme-image').should('be.visible');
-    typeTextAndCheckItsPosition();
+    typeTextAndCheckItsPosition('#meme-image', '#meme-text');
   });
 
-  it("O site deve ter uma moldura em volta da área onde a imagem aparecerá depois de ser escolhida. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.", function() {
+  it("O site deve ter uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.", function () {
     cy.viewport(1366, 768);
     cy.visit('/');
     cy.reload();
@@ -116,27 +116,78 @@ describe('Meme generator', function() {
     checkFullOverlappage('#meme-image-container', '#meme-image');
   });
 
+  it("O texto que será inserido sobre a imagem deve ter uma cor, sombra e tamanho específicos.", function () {
+    cy.viewport(1366, 768);
+    cy.visit('/');
+    cy.reload();
+
+    // Image container has a white background
+    cy.get('#meme-text').should($text => {
+      expect($text).to.have.css('text-shadow', 'rgb(0, 0, 0) 5px 5px 5px');
+    });
+
+    cy.get('#meme-text').should(
+      'have.css',
+      'font-size',
+      '30px',
+    );
+
+    cy.get('#meme-text').should(
+      'have.css',
+      'color',
+      'rgb(255, 255, 255)',
+    );
+
+    // Image is precisely inside it's container
+    memeUpload();
+    checkFullOverlappage('#meme-image-container', '#meme-image');
+  });
+
+  it("Limite o tamanho do texto que o usuário pode inserir.", function () {
+    cy.get('#text-input')
+      .type('I have written a line that has precisely sixty-one characters')
+      .should(
+        'have.value',
+        'I have written a line that has precisely sixty-one character',
+      );
+  });
+
   // Bonus requirements
 
-  it("Permita a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas. A página deve ter três botões, cada um colocando a própria borda ao redor da imagem. Um botão identificado com o id 'button1' deve estilizar o container da imagem com uma borda de 3 pixels, _dashed_ e vermelha. O botão com id 'button2' deve ser azul, borda _double_ de 5 pixels. O botão com id 'button3' deve ser verde com borda _groove_ com 6 pixels.", function() {
+  it("Permita a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas. A página deve ter três botões, que ao serem clicados devem cada um trocar a própria borda ao redor do container.", function () {
     cy.viewport(1366, 768);
     cy.visit('/');
 
-    cy.get('#button1').click();
+    cy.get('#fire').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 0, 0)',
+    );
+    cy.get('#fire').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
       '3px dashed rgb(255, 0, 0)',
     );
 
-    cy.get('#button2').click();
+    cy.get('#water').should(
+      'have.css',
+      'background-color',
+      'rgb(0, 0, 255)',
+    );
+    cy.get('#water').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
       '5px double rgb(0, 0, 255)',
     );
 
-    cy.get('#button3').click();
+    cy.get('#earth').should(
+      'have.css',
+      'background-color',
+      'rgb(0, 128, 0)',
+    );
+    cy.get('#earth').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
@@ -144,7 +195,7 @@ describe('Meme generator', function() {
     );
   });
 
-  it("Tenha um conjunto de quatro imagens pré prontas de memes famosos para o usuário escolher. Liste miniaturas das imagens e, mediante clique do usuário, essa imagem deve aparecer dentro da moldura para ser customizada. O elemento clicável deve ser identificado um um id denominado 'meme-1' para o primeiro meme, 'meme-2' para o segundo, e assim por diante.", function() {
+  it("Tenha um conjunto de quatro imagens pré prontas de memes famosos para o usuário escolher. Mostre miniaturas das imagens e, mediante clique do usuário, essa imagem deve aparecer dentro da moldura do elemento de container.", function () {
     cy.viewport(1366, 768);
     cy.visit('/');
 
@@ -152,16 +203,7 @@ describe('Meme generator', function() {
       cy.get(`#meme-${memeId}`).click();
       cy.get('#meme-image')
         .should('have.attr', 'src')
-        .and('match', new RegExp(`imgs/meme${memeId}.jpeg$`));
+        .and('match', new RegExp(`imgs/meme${memeId}.png$`));
     });
-  });
-
-  it('Limite o tamanho do texto que o usuário pode inserir. A quantidade máxima de caracteres deve ser 60.', function() {
-    cy.get('#text-input')
-      .type('I have written a line that has precisely sixty-one characters')
-      .should(
-        'have.value',
-        'I have written a line that has precisely sixty-one character',
-      );
   });
 });
