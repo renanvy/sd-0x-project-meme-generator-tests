054780967c1a68167a9036faa1cec5962bee08b0
Bruno Batista <brunobatista101@gmail.com>
2021-02-26 15:52:16 -0300

    ajustes testes

--- cypress/integration/meme-generator.spec.js
+++ cypress/integration/meme-generator.spec.js
@@ -45,7 +45,6 @@ const checkFullOverlappage = (backgroundSelector, forefrontSelector) => {
 
     expect(isOverEachother(backgroundElement, forefrontElement)).to.be.true;
 
-    // Text has volume
     expect(forefrontElement.top).to.not.be.undefined;
     expect(forefrontElement.bottom).to.not.be.undefined;
     expect(forefrontElement.left).to.not.be.undefined;
@@ -56,12 +55,8 @@ const checkFullOverlappage = (backgroundSelector, forefrontSelector) => {
 };
 
 const typeTextAndCheckItsPosition = (container, child) => {
-  // It inserts text in the input and page when typed
   typeText();
-
   checkTypedTextIsVisible();
-
-  // Has one element inside another
   checkFullOverlappage(container, child);
 };
 
@@ -88,7 +83,7 @@ const memeUpload = () => {
 
 //-------
 
-describe("O site deve possuir uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida.", () => {
+describe("1 - Crie uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -110,7 +105,7 @@ describe("O site deve possuir uma caixa de texto com a qual quem usa pode intera
   })
 });
 
-describe("O site deve permitir que quem usa faça upload de uma imagem de seu computador.", () => {
+describe("2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -132,7 +127,7 @@ describe("O site deve permitir que quem usa faça upload de uma imagem de seu co
   })
 });
 
-describe("O site deve ter uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.", () => {
+describe("3 - Adicione uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -140,7 +135,6 @@ describe("O site deve ter uma moldura no container. A moldura deve ter 1 pixel d
   })
 
   it("O elemento que serve de container para a imagem deve ter o a cor de fundo branca", () => {
-    // Image container has a white background
     cy.get('#meme-image-container').should($container => {
       expect($container).to.have.css('background-color', 'rgb(255, 255, 255)');
     });
@@ -155,13 +149,12 @@ describe("O site deve ter uma moldura no container. A moldura deve ter 1 pixel d
   })
 
   it("A imagem deve estar totalmente contida dentro do elemento identificado como `meme-image-container`", () => {
-    // Image is precisely inside it's container
     memeUpload();
     checkFullOverlappage('#meme-image-container', '#meme-image');
   })
 });
 
-describe("O texto que será inserido sobre a imagem deve ter uma cor, sombra e tamanho específicos.", () => {
+describe("4 - Adicione o texto que será inserido sobre a imagem deve ter uma cor, sombra e tamanho específicos.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -175,10 +168,7 @@ describe("O texto que será inserido sobre a imagem deve ter uma cor, sombra e t
   })
 
   it("O texto do elemento `meme-text` deve ter a fonte com o tamanho de 30 pixels", () => {
-    cy.get('#meme-text').should(
-      'have.css',
-      'font-size',
-      '30px',
+    cy.get('#meme-text').should('have.css','font-size','30px',
     );
   })
 
@@ -191,7 +181,7 @@ describe("O texto que será inserido sobre a imagem deve ter uma cor, sombra e t
   })
 });
 
-describe("Limite o tamanho do texto que o usuário pode inserir.", () => {
+describe("5 - Limite o tamanho do texto que o usuário pode inserir.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -210,7 +200,7 @@ describe("Limite o tamanho do texto que o usuário pode inserir.", () => {
 
 // Bonus requirements
 
-describe("Permita a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas. A página deve ter três botões, que ao serem clicados devem cada um trocar a própria borda ao redor do container.", () => {
+describe("6 - Permita a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas. A página deve ter três botões, que ao serem clicados devem cada um trocar a própria borda ao redor do container.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
@@ -259,7 +249,7 @@ describe("Permita a quem usa customizar o meme escolhido acrescentando a ele uma
   })
 });
 
-describe("Tenha um conjunto de quatro imagens pré prontas de memes famosos para o usuário escolher. Mostre miniaturas das imagens e, mediante clique do usuário, essa imagem deve aparecer dentro da moldura do elemento de container.", () => {
+describe("7 - Tenha um conjunto de quatro imagens pré prontas de memes famosos para o usuário escolher. Mostre miniaturas das imagens e, mediante clique do usuário, essa imagem deve aparecer dentro da moldura do elemento de container.", () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
